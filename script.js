8d2fafbaae77f85ffc8abbeecc98a431ee59652d
Tomasz <tomasz.iwanicki@onet.pl>
2022-07-09 19:00:17 +0200

    Add logic so that you can't choose multiple chars

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const res = document.getElementById("display-message")
 let result = ""
 
+const checkIfChar = () => {
+    if (result[result.length - 1] !== '+' &&
+        result[result.length - 1] !== '-' &&
+        result[result.length - 1] !== '*' &&
+        result[result.length - 1] !== '/' &&
+        result[result.length - 1] !== ','
+    ) return true
+}
+
 document.querySelector("#n0").addEventListener("click", () => {
     result += "0"
     res.innerHTML = result
@@ -44,28 +53,28 @@ document.querySelector("#n9").addEventListener("click", () => {
 
 document.querySelector("#add").addEventListener("click", () => {
     if (result.length !== 0) {
-        result += "+"
+        if (checkIfChar()) result += "+"
         document.getElementById("display-message").innerHTML = result
     } else
         document.getElementById("display-message").innerHTML = "0"
 })
 document.querySelector("#subtract").addEventListener("click", () => {
     if (result.length !== 0) {
-        result += "-"
+        if (checkIfChar()) result += "-"
         document.getElementById("display-message").innerHTML = result
     } else
         document.getElementById("display-message").innerHTML = "0"
 })
 document.querySelector("#multiply").addEventListener("click", () => {
     if (result.length !== 0) {
-        result += "*"
+        if (checkIfChar()) result += "*"
         document.getElementById("display-message").innerHTML = result
     } else
         document.getElementById("display-message").innerHTML = "0"
 })
 document.querySelector("#divide").addEventListener("click", () => {
     if (result.length !== 0) {
-        result += "/"
+        if (checkIfChar()) result += "/"
         document.getElementById("display-message").innerHTML = result
     } else
         document.getElementById("display-message").innerHTML = "0"
@@ -73,7 +82,7 @@ document.querySelector("#divide").addEventListener("click", () => {
 
 const comma = document.querySelector("#comma").addEventListener("click", () => {
     if (result.length !== 0) {
-        result += ","
+        if (checkIfChar()) result += ","
         document.getElementById("display-message").innerHTML = result
     } else
         document.getElementById("display-message").innerHTML = "0"
@@ -92,6 +101,11 @@ document.querySelector("#backspace").addEventListener("click", () => {
 
 
 document.querySelector("#equals").addEventListener("click", () => {
-    console.log(result)
-
+    if (checkIfChar()) {
+        const firstNumber = parseFloat(result.slice(0, result.indexOf("+")))
+        const secondNumber = parseFloat(result.slice(result.indexOf("+") + 1, result.length))
+        console.log(firstNumber)
+        console.log(secondNumber)
+        console.log(firstNumber + secondNumber)
+    }
 })
